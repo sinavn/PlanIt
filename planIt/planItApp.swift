8f56a97bb9f85406fe4084c620d6d78dd4938db8
sinavn <vosogh858sina@gmail.com>
2023-10-19 23:43:55 +0330

    Initial Commit

---
+++ planIt/planItApp.swift
@@ -0,0 +1,17 @@
+//
+//  planItApp.swift
+//  planIt
+//
+//  Created by SinaVN on 7/27/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct planItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
